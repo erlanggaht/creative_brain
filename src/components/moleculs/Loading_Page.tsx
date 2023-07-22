5ccefc57b476089ddbc5263151cfbe40cb63b7e2
erlanggaht <erlanggahidayat.md@gmail.com>
2023-07-22 23:07:33 +0700

    kuy

--- src/components/moleculs/Loading_Page.tsx
+++ src/components/moleculs/Loading_Page.tsx
@@ -2,9 +2,9 @@
 export default function Loading_Page() {
     return (
         <>
-            <div className="loading-container p-1 ">
-                <div className="loader">
-                    <div className="loading-slide">
+            <div className="loading-container p-1">
+                <div className="loader ">
+                    <div className="loading-slide ">
                         <div className="slide"></div>
                         <div className="slide"></div>
                         <div className="slide"></div>
