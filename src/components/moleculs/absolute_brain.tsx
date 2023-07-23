ac35cf3ed4600cf8611503ae7afcec9a1bc0203b
erlanggaht <erlanggahidayat.md@gmail.com>
2023-07-23 13:59:49 +0700

    okeh

--- src/components/moleculs/absolute_brain.tsx
+++ src/components/moleculs/absolute_brain.tsx
@@ -16,17 +16,17 @@ export default function Absolute_Brain() {
 export function InBrain() {
     return (
         <>
-            <div className="absolute top-[86px] md:font-[400] font-[200] cursor-pointer left-[60px] px-1 text-white z-50 drop-shadow -rotate-12 bg-sky-900 scale-90" >ReactJs</div>
-            <div className="absolute top-[47px] md:font-[400] font-[200] cursor-pointer left-[80px] px-1 text-white z-50 drop-shadow rotate-2 bg-[#222]" >NextJS</div>
-            <div className="absolute top-[119px] md:font-[400] font-[200] cursor-pointer left-[90px] px-1 text-white z-50 drop-shadow -rotate-6 bg-green-700 " >VueJs</div>
-            <div className="absolute top-[180px] md:font-[400] font-[200] cursor-pointer left-[40px] px-1 text-sky-800 z-50 drop-shadow -rotate-6 bg-white" >Tailwinds</div>
+            <div  id="in_brain_link"  className="absolute top-[86px] md:font-[400] font-[200] cursor-pointer left-[60px] px-1 text-white z-50 drop-shadow -rotate-12 bg-sky-900 scale-90" >ReactJs</div>
+            <div  id="in_brain_link"  className="absolute top-[47px] md:font-[400] font-[200] cursor-pointer left-[80px] px-1 text-white z-50 drop-shadow rotate-2 bg-[#222]" >NextJS</div>
+            <div  id="in_brain_link"  className="absolute top-[119px] md:font-[400] font-[200] cursor-pointer left-[90px] px-1 text-white z-50 drop-shadow -rotate-6 bg-green-700 " >VueJs</div>
+            <div  id="in_brain_link"  className="absolute top-[180px] md:font-[400] font-[200] cursor-pointer left-[40px] px-1 text-sky-800 z-50 drop-shadow -rotate-6 bg-white" >Tailwinds</div>
 
-            <div className="absolute top-[36px] md:font-[400] font-[200] cursor-pointer right-[60px] px-1 text-black z-50 drop-shadow rotate-12 bg-white" >Expressjs</div>
-            <div className="absolute top-[97px] md:font-[400] font-[200] cursor-pointer right-[10px] px-1 text-white z-50 drop-shadow rotate-45 bg-red-800" >NestJs</div>
-            <div className="absolute top-[138px] md:font-[400] font-[200] cursor-pointer right-[60px] px-1 text-white z-50 drop-shadow rotate-12 bg-yellow-400 " >Django</div>
-            <div className="absolute top-[200px] md:font-[400] font-[200] cursor-pointer right-[80px] px-1 text-white z-50 drop-shadow rotate-1 bg-green-800" >MongoDB</div>
-            <div className="absolute top-[168px] md:font-[400] font-[200] cursor-pointer right-[10px] px-1 text-yellow-400 z-50 drop-shadow -rotate-45 bg-white scale-75" >MySql</div>
-            <div className="absolute top-[90px] scale-75 bg-neutral-400 md:font-[400] font-[200] cursor-pointer right-[60px] px-1 text-gray-200 z-50 drop-shadow rotate-2 " >PostgresSQL</div>
+            <div id="in_brain_link" className="absolute top-[36px] md:font-[400] font-[200] cursor-pointer right-[60px] px-1 text-black z-50 drop-shadow rotate-12 bg-white" >Expressjs</div>
+            <div id="in_brain_link" className="absolute top-[97px] md:font-[400] font-[200] cursor-pointer right-[10px] px-1 text-white z-50 drop-shadow rotate-45 bg-red-800" >NestJs</div>
+            <div id="in_brain_link" className="absolute top-[138px] md:font-[400] font-[200] cursor-pointer right-[60px] px-1 text-white z-50 drop-shadow rotate-12 bg-yellow-400 " >Django</div>
+            <div id="in_brain_link" className="absolute top-[200px] md:font-[400] font-[200] cursor-pointer right-[80px] px-1 text-white z-50 drop-shadow rotate-1 bg-green-800" >MongoDB</div>
+            <div id="in_brain_link" className="absolute top-[168px] md:font-[400] font-[200] cursor-pointer right-[10px] px-1 text-yellow-400 z-50 drop-shadow -rotate-45 bg-white scale-75" >MySql</div>
+            <div id="in_brain_link" className="absolute top-[90px] scale-75 bg-neutral-400 md:font-[400] font-[200] cursor-pointer right-[60px] px-1 text-gray-200 z-50 drop-shadow rotate-2 " >PostgresSQL</div>
         </>
     )
 }
