2821689bf4989a5a98b9c3c8e479b0dcd69ffa24
erlanggaht <erlanggahidayat.md@gmail.com>
2023-08-10 13:39:28 +0700

    yah

--- src/assets/imported/audio.tsx
+++ src/assets/imported/audio.tsx
@@ -1,6 +1,6 @@
 import arrow_wosh from '@/assets/arrow_wosh.wav' 
-import join from '@/assets/join.wav' 
-import logout from '@/assets/logout.wav' 
+import join from '@/assets/join.mp3' 
+import logout from '@/assets/logout.mp3' 
 import send from '@/assets/send.mp3' 
 import submit_guest from '@/assets/submit_guest.mp3' 
 
