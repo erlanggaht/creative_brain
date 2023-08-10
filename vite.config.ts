1940bfdfcde0d784a89589578bc43a32ae9b6750
erlanggaht <erlanggahidayat.md@gmail.com>
2023-08-11 01:10:12 +0700

    ojeh

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import dotenv from 'dotenv'
 dotenv.config() // load env vars from .env
+import { VitePWA } from 'vite-plugin-pwa'
 
 const DEFAULT_OPTIONS = {
   test: /\.(jpe?g|png|gif|tiff|webp|avif)$/i,
@@ -72,7 +73,33 @@ export default defineConfig({
   plugins: [
     react(),
     ViteImageOptimizer(DEFAULT_OPTIONS),
-   
+    VitePWA({ 
+    registerType: 'autoUpdate', 
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: 'Brain./Me',
+      short_name: 'Erlangga Hidayatullah',
+      description: 'Erlangga Hidayatullah website portfolio made with reactjs and tailwind technology plus a UI library called flowbite',
+      theme_color: '#11101a',
+      icons: [
+        {
+          src: '/vite.ico',
+          sizes: '192x192',
+          type: 'image/ico'
+        },
+        {
+          src: '/vite.ico',
+          sizes: '512x512',
+          type: 'image/ico'
+        }
+      ]
+    }
+  })
 ],
   define: {
     __HOST__: `"${process.env.HOST}"`,
